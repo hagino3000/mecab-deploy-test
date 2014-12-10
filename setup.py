89a18ec14c01fb16f58c2838f783c7e4ca102ac2
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-12-10 05:25:32 +0000

    Mod setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Takashi Nishibayashi(hagino3000)',
     author_email="takashi.nishibayashi@gmail.com",
     description=short_description,
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=[],
     url='https://github.com/hagino3000/xxxxxx',
     packages=['myapp'],
     classifiers=[
